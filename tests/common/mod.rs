9ca85aa697c696ff467afec3a050b887b208463d
HenriG <49301770+hGrigol@users.noreply.github.com>
2019-07-05 08:34:32 +0200

    tests für pointer frei erweitert, mod.rs angepasst

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,4 @@
+use wavelet5::WaveletTreePointerFree;
 use wavelet5::WaveletTree;
 pub fn setup_string1() -> WaveletTree<char> {
     wavelet5::WaveletTree::create_tree("Hallo Ich bin ein Test für einen Satz".chars().into_iter())
@@ -7,6 +8,14 @@ pub fn setup_string() -> WaveletTree<char> {
     wavelet5::WaveletTree::create_tree("AsWDaaaaa aGDW!/(%§".chars().into_iter())
 }
 
+pub fn setup_string_2_2() -> WaveletTreePointerFree<char> {
+    wavelet5::WaveletTreePointerFree::create("Hallo Ich bin ein Test für einen Satz".chars().into_iter())
+}
+
+pub fn setup_string_2_1() -> WaveletTreePointerFree<char> {
+    wavelet5::WaveletTreePointerFree::create("AsWDaaaaa aGDW!/(%§".chars().into_iter())
+}
+
 pub fn setup_ints() -> WaveletTree<u64> {
     let mut vec = Vec::new();
     for x in 0..1000000 {
@@ -14,3 +23,6 @@ pub fn setup_ints() -> WaveletTree<u64> {
     }
     wavelet5::WaveletTree::create_tree(vec.into_iter())
 }
+
+
+
--- tests/wavelet_tree_pointer_free_test.rs
+++ tests/wavelet_tree_pointer_free_test.rs
@@ -1,5 +1,5 @@
 use serde_json;
-use wavelet5::{Error, WaveletTreePointerFree};
+use wavelet5::{Error_Pointer_Free, WaveletTreePointerFree};
 #[macro_use(matches)]
 extern crate matches;
 mod common;
@@ -12,7 +12,7 @@ fn testing_string_access_in_sequence() {
     assert_eq!(tree.access(1).unwrap(), 'H');
 }
 
-/*#[test]
+
 fn testing_string_access_in_sequence2() {
     let tree = common::setup_string_2_2();
     assert_eq!(tree.access(37).unwrap(), 'z');
@@ -22,9 +22,9 @@ fn testing_string_access_in_sequence3() {
     let tree = common::setup_string_2_2();
     assert_eq!(tree.access(5).unwrap(), 'o');
 }
-*/
 
-/*
+
+
 #[test]
 fn testing_string_access_not_in_sequence() {
     let tree = common::setup_string_2_1();
@@ -38,5 +38,5 @@ fn testing_string_access_0() {
 
     assert!(matches!(tree.access(0), Err(Error_Pointer_Free::Access0)));
 }
-*/
+
 
