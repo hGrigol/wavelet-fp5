4ca3be6359fd6e38adaa1b4abcbe85864d5cb0f9
Niklas Haldorn <49301848+Daelam@users.noreply.github.com>
2019-07-05 08:17:04 +0200

    changed lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 pub mod wavelet_tree_pointer;
+pub mod wavelet_tree_pointer_free;
 pub use wavelet_tree_pointer::Error;
 pub use wavelet_tree_pointer::WaveletTree;
+pub use wavelet_tree_pointer_free::Error_Pointer_Free;
+pub use wavelet_tree_pointer_free::WaveletTreePointerFree;
